59a09331fea9aaceff42657df18816003ddefa69
Lasith-Niro <lasith2013.l2n@gmail.com>
2020-05-08 01:09:24 +0100

    mini map toggle added

---
+++ Assets/Scripts/ToggleMiniMap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMiniMap : MonoBehaviour
+{
+    public GameObject miniMap;
+    private bool isShow = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isShow)
+            {
+                Show();
+            }
+            else
+            {
+                Hide();
+            }
+        }
+    }
+
+    void Show()
+    {
+        miniMap.SetActive(true);
+        isShow = false;
+    }
+
+    void Hide()
+    {
+        miniMap.SetActive(false);
+        isShow = true;
+    }
+}
