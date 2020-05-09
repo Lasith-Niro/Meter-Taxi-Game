6c171f4e6ffdc8350c810ad0318612234af2180d
Lasith-Niro <lasith2013.l2n@gmail.com>
2020-05-09 20:15:13 +0100

    simple AI script added to make traffic on the city

---
+++ Assets/Scripts/SelfController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SelfController : MonoBehaviour
+{
+    public Transform path;
+    private float speed;
+    private List<Transform> nodes;
+    private int rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = UnityEngine.Random.Range(0, 4);
+        Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != path.transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+        rand = UnityEngine.Random.Range(0, nodes.Count);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nodes[rand].position, speed * Time.deltaTime);
+        transform.LookAt(nodes[rand]);
+        if(Vector3.Distance(transform.position, nodes[rand].position) < 0.2f)
+        {
+            rand = UnityEngine.Random.Range(0, nodes.Count);
+        }
+    }
+}
